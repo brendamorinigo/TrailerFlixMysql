753e6226a047bb5ff17138560088ec834dfc46ef
brendamorinigo <90360932+brendamorinigo@users.noreply.github.com>
2025-07-24 00:35:02 -0300

    Desarrollo de server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const Sequelize = require('sequelize');
-const express= require('express');
+const express = require('express');
+const router = express.Router();
+const app = express();
 
+app.use(express.json()); 
+app.use(require('./routs/actores')); 
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en puerto 3000');
+});
+
+const Actores = require('../models/actores');
+
+app.post('/actores', async (req, res) => {
+  try {
+    const { nombreCompleto } = req.body;
+    const nuevoActor = await Actores.create({ nombreCompleto });
+
+    res.status(201).json(nuevoActor);
+  } catch (error) {
+    console.error('Error al crear el actor:', error);
+    res.status(500).json({ error: 'Error al crear el actor' });
+  }
+});
+
+module.exports = router;
